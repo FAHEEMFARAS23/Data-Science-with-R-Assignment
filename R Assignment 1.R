18c421d85523486d7c1a002fe718f9e7b561e041
FAHEEMFARAS23 <faheemfarasfhm@gmail.com>
2021-01-22 14:21:29 +0000

    r

---
+++ R Assignment 1.R
@@ -0,0 +1,22 @@
+# R as a Calculator
+# addition
+22 + 14
+
+#Subtraction
+138 - 87
+
+# Multiplication
+354 * 23
+
+#Division
+480 / 6
+
+# Trignometry
+sin (45)
+
+log (100)
+
+#Powers
+12 ^ 3
+
+
